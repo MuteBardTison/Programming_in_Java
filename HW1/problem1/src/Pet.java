bd9cfd5ee77e082c3ecda6820ffeea04ee9d64e5
MuteBardTison <z.qi@jacobs-university.de>
2018-03-04 00:38:16 +0100

    HW1

---
+++ HW1/problem1/src/Pet.java
@@ -0,0 +1,112 @@
+/**
+ * This class represents a pet object
+ * 
+ * Naming accords to Google Java Style
+ * 
+ * @author Zihan Qi
+ */
+
+package src;
+
+public class Pet{
+    private String name;        // Name of pet
+    private String species;     // Species of pet
+    private double height;      // Height of pet in meters
+    private double weight;      // Weight of pet in kilograms
+
+    /**
+     * Constructor of pet instance
+     * @param name name of pet to be constructed
+     * @param species species of pet to be constructed
+     * @param height height of pet to be constructed
+     * @param weight weight of pet to be constructed
+     */
+    public Pet(String name, String species, double height, double weight) {
+        this.name = name;
+        this.species = species;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + this.name
+            + "\nSpecies: " + this.species
+            + "\nHeight: " + this.height
+            + "\nWeight: " + this.weight;
+    }
+
+    @Override
+	public boolean equals(Object o){
+		if (o instanceof Pet){
+			Pet pet = (Pet) o;
+            return (this.name == pet.name) && (this.species == pet.species)
+            && (this.height == pet.height) && (this.weight == pet.weight);
+		}
+		return false;
+	}
+
+    /**
+     * Getting method which returns the name of pet
+     * @return name of pet
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Getting method which returns the species of pet
+     * @return species of pet
+     */
+    public String getSpecies() {
+        return species;
+    }
+
+    /**
+     * Getting method which returns the height of pet
+     * @return height of pet
+     */
+    public double getHeight() {
+        return height;
+    }
+
+    /**
+     * Getting method which returns the weight of pet
+     * @return weight of pet
+     */
+    public double getWeight() {
+        return weight;
+    }
+
+    /**
+     * Setting method which sets the name of pet
+     * @param newName new name of pet
+     */
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    /**
+     * Setting method which sets the species of pet
+     * @param newSpecies new species of pet
+     */
+    public void setSpecies(String newSpecies) {
+        this.species = newSpecies;
+    }
+
+    /**
+     * Setting method which sets the height of pet
+     * @param newHeight new height of pet
+     */
+    public void setHeight(double newHeight) {
+        this.height = newHeight;
+    }
+
+    /**
+     * Setting method which sets the weight of pet
+     * @param newWeight new weight of pet
+     */
+    public void setWeight(double newWeight) {
+        this.weight = newWeight;
+    }
+}
---
+++ HW1/problem1/test/PetDriver.java
@@ -0,0 +1,42 @@
+/**
+ * This class represents a driver for pet class
+ * which test the functionalities of a pet instance
+ * 
+ * Naming accords to Google Java Style
+ * 
+ * @author Zihan Qi
+ */
+
+package test;
+import src.Pet;
+
+public class PetDriver{
+    /**
+	 * Tests functionalities of {@link Pet} class.
+	 * @param args command line arguments
+	 */
+    public static void main(String[] args) {
+        /* Constructor test*/
+        Pet petOne = new Pet("Charlie", "Dog", 0.5, 40.0);
+        System.out.println("Pet One:\n" + petOne + "\n");
+        Pet petTwo = new Pet("Kitty", "Cat", 0.2, 20.0);
+        System.out.println("Pet Two:\n" + petTwo + "\n");
+        /* Getting methods test*/
+        System.out.println("Getting Methods:");
+        System.out.println("Get Name for Pet One: " + petOne.getName());
+        System.out.println("Get Species for Pet One: " + petOne.getSpecies());
+        System.out.println("Get Height for Pet One: " + petOne.getHeight());
+        System.out.println("Get Weight for Pet One: " + petOne.getWeight());
+        /* Setting methods test*/
+        System.out.println("\nSetting Methods:");
+        petTwo.setName("Daisy");
+        petTwo.setSpecies("Monkey");
+        petTwo.setHeight(0.8);
+        petTwo.setWeight(30.0);
+        System.out.println("Pet Two:\n" + petTwo + "\n");
+        /* Equality test*/
+        System.out.println("Pet One equals Pet Two: " + petOne.equals(petTwo));
+        Pet petThree = new Pet("Charlie", "Dog", 0.5, 40.0);
+        System.out.println("Pet One equals Pet Three: " + petOne.equals(petThree));
+    }
+}
